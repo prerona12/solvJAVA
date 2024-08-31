a9a324071b403daaa486b4f7e2c6d70b3362a7f6
prerona12 <147043147+prerona12@users.noreply.github.com>
2024-08-31 20:18:01 +0530

    Create asciivalue.java

---
+++ asciivalue.java
@@ -0,0 +1,11 @@
+//asciee value converter 
+
+import java.util.*;
+public class Main{
+    public static void main(String arg[]){
+        Scanner input = new Scanner(System.in);
+        char c = input.next().charAt(0);
+        int as = c;
+        System.out.println(as);
+    }
+}
