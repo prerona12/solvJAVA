02bdbd641ffd3c87a8db5ed13a3c503705aed38c
prerona12 <147043147+prerona12@users.noreply.github.com>
2025-04-11 13:35:14 +0530

    Update reversestring.java

--- reversestring.java
+++ reversestring.java
@@ -13,3 +13,22 @@ public class Main
 		
 	}
 }
+
+//when array size is not given 
+import java.util.*;
+public class Main
+{
+	public static void main(String[] args) {
+		Scanner input = new Scanner(System.in);
+		String str = input.nextLine();
+		String num[] = str.split(" ");
+		ArrayList<Integer> list = new ArrayList<>();
+		for (int i = num.length - 1; i >= 0; i--) {
+            list.add(Integer.parseInt(num[i]));
+        }
+		for(int e : list){
+		    System.out.print(e + " ");
+		}
+		
+	}
+}
