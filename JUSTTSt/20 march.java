6045064ba26e07bbf1eff8216f4b312b460cdeb7
prerona12 <147043147+prerona12@users.noreply.github.com>
2025-04-15 16:08:03 +0530

    Create 20 march.java

---
+++ JUSTTSt/20 march.java
@@ -0,0 +1,28 @@
+//sum of all prime number in the given range 
+
+import java.util.*;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+        int last = input.nextInt();
+        int sum = 0;
+
+        for (int i = 2; i <= last; i++) {
+            if (isPrime(i)) {
+                sum += i;
+            }
+        }
+
+        System.out.println(sum);
+    }
+
+    // Helper method to check for primality
+    public static boolean isPrime(int n) {
+        if (n <= 1) return false;
+        for (int i = 2; i <= Math.sqrt(n); i++) {
+            if (n % i == 0) return false;
+        }
+        return true;
+    }
+}
