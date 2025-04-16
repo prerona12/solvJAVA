59c9f9ddb810f4a460d00fc61c6aa0b5fb47221c
prerona12 <147043147+prerona12@users.noreply.github.com>
2025-04-16 21:06:47 +0530

    Update frequency.java

--- frequency.java
+++ frequency.java
@@ -30,3 +30,31 @@ public class Main {
         }
     }
 }
+
+//hashmap easy but complicated 
+
+import java.util.*;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+        String[] str = input.nextLine().split(" ");
+        
+        ArrayList<Integer> list = new ArrayList<>();
+        for (String s : str) {
+            list.add(Integer.parseInt(s));
+        }
+
+        // Frequency map
+        HashMap<Integer, Integer> freqMap = new HashMap<>();
+
+        for (int num : list) {
+            freqMap.put(num, freqMap.getOrDefault(num, 0) + 1);
+        }
+
+        // Display frequencies
+        for (Map.Entry<Integer, Integer> entry : freqMap.entrySet()) {
+            System.out.println(entry.getKey() + " occurs " + entry.getValue() + " times");
+        }
+    }
+}
